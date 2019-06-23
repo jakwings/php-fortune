be86a8934b25d3cce443657b5e1f9b8b15c7421e
J.W <jakwings@gmail.com>
2019-06-24 07:56:29 +0800

    Add 10 more test cases

--- test.php
+++ test.php
@@ -7,12 +7,13 @@ $filepath = 'fortunes/data.txt';
 
 $fortune->CreateIndexFile($filepath);
 
-assert($fortune->GetNumberOfQuotes($filepath) === 5);
+assert($fortune->GetNumberOfQuotes($filepath) === 6);
 assert($fortune->GetExactQuote($filepath, 0) === "Hello, world.\r\n");
 assert($fortune->GetExactQuote($filepath, 1) === "The second quote.\n");
 assert($fortune->GetExactQuote($filepath, 2) === "The third quote.\n");
 assert($fortune->GetExactQuote($filepath, 3) === "The fourth quote.\n");
-assert($fortune->GetExactQuote($filepath, 4) === "Goodbye.");
+assert($fortune->GetExactQuote($filepath, 4) === "[V] need your money!\n");
+assert($fortune->GetExactQuote($filepath, 5) === "Goodbye.");
 
 echo $fortune->GetRandomQuote($filepath);
 
